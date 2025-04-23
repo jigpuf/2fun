c82055604d05a0df1c49fac906c2223ed2cb3f44
jigpuf <32025384+jigpuf@users.noreply.github.com>
2025-04-23 18:54:35 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ const Main = (props) => {
   const activeTab = props.activeTab;
   return (
     <div className="main">
+      hi
       {latitude !== null && longitude !== null ? (
         <table>
           <tr>
